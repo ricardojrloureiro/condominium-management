e687c932cfa56ae3aa67927d59698fb911974c12
Rui Gomes <ruigomeseu@gmail.com>
2013-10-10 22:18:53 +0100

    Test.cpp rename

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include "condominium.h"
+#include "property.h"
+
+
+int main(){
+	Condominium teste("And");
+	Apartment mansaomelaca("rua do melo");
+	teste.addProperty(mansaomelaca);
+
+	//runSuite();
+    return 0;
+}
