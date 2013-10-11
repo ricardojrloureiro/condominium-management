7277bde2616ac79226f617aeb2cd8bb4897bf8c3
Ricardo Loureiro <ricardojrloureiro@gmail.com>
2013-10-11 11:31:02 +0100

    Blank line removal

--- main.cpp
+++ main.cpp
@@ -1,13 +1,10 @@
 #include "condominium.h"
 #include "property.h"
 
-
 int main(){
 	Condominium teste("And");
 	Apartment mansaomelaca("rua do melo");
 	teste.addProperty(mansaomelaca);
-
-
 
     return 0;
 }
