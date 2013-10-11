d619b1c17f0383655cb12024daefe4e371187e35
Ricardo Loureiro <ricardojrloureiro@gmail.com>
2013-10-11 11:37:58 +0100

    penis comment

--- main.cpp
+++ main.cpp
@@ -1,10 +1,10 @@
 #include "condominium.h"
 #include "property.h"
-
+c
 int main(){
 	Condominium teste("And");
 	Apartment mansaomelaca("rua do melo");
 	teste.addProperty(mansaomelaca);
-
+	 // penis fofinho
     return 0;
 }
